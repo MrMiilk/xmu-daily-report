25b9a5a4362bd4f0578ba9fdf545130e4bfb56b5
MrMiilk <40832261+MrMiilk@users.noreply.github.com>
2022-10-20 11:37:03 +0800

    Update XMUAutoCheckIn.py

--- XMUAutoCheckIn.py
+++ XMUAutoCheckIn.py
@@ -89,15 +89,6 @@ def checkin(cfg: Config, use_vpn=True) -> None:
     job.add_child(
         dropdown_province("福建省"),
         dropdown_city("厦门市"),
-        dropdown_district(cfg.district),
-        dropdown_inschool(cfg.inschool).add_child(
-            dropdown_campus(cfg.campus),
-            dropdown_stay_in_school("住校内"),
-            dropdown_indorm("住校内学生宿舍"),
-            dropdown_building(cfg.building),
-            text_room(cfg.room)
-        ) if cfg.inschool.startswith("在校") else
-        dropdown_inschool(cfg.inschool),
         dropdown_covid_test(),
         dropdown_confirm(),
         click_save()
